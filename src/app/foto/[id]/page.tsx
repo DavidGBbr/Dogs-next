002d8c7ce2551602c7368b153f7b27ea1b756ced
davidGBbr <davidgomesbrigido@gmail.com>
2024-09-30 21:22:23 -0300

    Photo Content

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,10 +1,31 @@
+import photoGet from "@/actions/photo-get";
+import PhotoContent from "@/components/photo/photo-content";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const FotoIdPage = ({ params }: { params: { id: number } }) => {
+type FotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: FotoIdParams) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return { title: "Fotos" };
+  return {
+    title: data.photo.title,
+  };
+}
+
+const FotoIdPage = async ({ params }: { params: { id: string } }) => {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return notFound();
   return (
-    <main>
-      <h1>Foto Id: {params.id}</h1>
-    </main>
+    <section className="container mainContainer">
+      <PhotoContent data={data} single={true} />
+    </section>
   );
 };
 
