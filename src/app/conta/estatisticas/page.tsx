0123cb585169dc10a13c47993c3a16a07b2d9d0b
davidGBbr <davidgomesbrigido@gmail.com>
2024-10-04 13:04:16 -0300

    Lazy Loading

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,12 +1,25 @@
+import statsGet from "@/actions/stats-get";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
+
+const ContaEstatisticas = dynamic(
+  () => import("@/components/conta/conta-estatisticas"),
+  {
+    loading: () => <p>Carregando...</p>,
+    ssr: false,
+  }
+);
 
 export const metadata: Metadata = {
   title: "Estatísticas | Minha Conta",
 };
-const EstatisticasPage = () => {
+const EstatisticasPage = async () => {
+  const { data } = await statsGet();
+
+  if (!data) return null;
   return (
     <main>
-      <h1>Estatisticas</h1>
+      <ContaEstatisticas data={data} />
     </main>
   );
 };
